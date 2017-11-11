5136034a00ecfa911c2306bdcbca83d9914926b9
YataFumika <vulpes1793@gmail.com>
2017-11-11 19:00:51 +0900

    jissyu #3 #4

--- lib/semver.rb
+++ lib/semver.rb
@@ -1,24 +1,42 @@
 class Semver
   def initialize(major, minor, patch)
-    argument_check(major)
-    argument_check(minor)
-    argument_check(patch)
+    validate major, minor, patch
 
     @major = major
     @minor = minor
     @patch = patch
   end
-  def version
+
+  def to_s
     "#{@major}.#{@minor}.#{@patch}"
   end
 
-  def equal other
-    version == other.version
+  def == other
+    to_s == other.to_s
+  end
+
+  def update_patch
+    self.class.new @major, @minor, @patch+1
+  end
+
+  def update_minor
+    self.class.new @major, @minor+1, 0
+  end
+
+  def update_major
+    self.class.new @major+1, 0, 0
   end
 
   private
 
+  def validate major, minor, patch
+    argument_check(major)
+    argument_check(minor)
+    argument_check(patch)
+  end
+
   def argument_check(val)
     raise ArgumentError.new if val < 0
+    raise ArgumentError.new unless val.instance_of? Integer
   end
 end
--- spec/semver_spec.rb
+++ spec/semver_spec.rb
@@ -1,6 +1,6 @@
 require_relative "../lib/semver"
 
-describe "Semver" do
+describe "バージョンオブジェクト" do
 
   describe "major, minor, patch 番号を与えてバージョンオブジェクトを作成" do
     it "1,2,3 を与えてバージョンオブジェクトを作成" do
@@ -9,14 +9,14 @@ describe "Semver" do
   end
 
   describe "バージョンオブジェクトの文字列表現を取得" do
-    it "1,2,3のバージョンオブジェクトでversionメソッドで'1.2.3'を取得する" do
+    it "major=1,minor=2,patch=3のバージョンオブジェクトのバージョン文字列'1.2.3'を返す" do
       semver = Semver.new 1, 2, 3
-      expect(semver.version).to eq  "1.2.3"
+      expect(semver.to_s).to eq  "1.2.3"
     end
 
-    it "1,4,2のバージョンオブジェクトでversionメソッドで'1.4.2'を取得する" do
+    it "major=1,minor=4,patch=2のバージョンオブジェクトのバージョン文字列'1.4.2'を返す" do
       semver = Semver.new 1, 4, 2
-      expect(semver.version).to eq  "1.4.2"
+      expect(semver.to_s).to eq  "1.4.2"
     end
   end
 
@@ -24,27 +24,76 @@ describe "Semver" do
     it "バージョン 1.4.2 は バージョン 2.1.0 と等しくない" do
       semver1 = Semver.new 1, 4, 2
       semver2 = Semver.new 2, 1, 0
-      expect(semver1.equal semver2).to eq false
+      expect(semver1 == semver2).to be_falsey
     end
     it "バージョン 1.4.2 は バージョン 1.4.2 と等しい" do
       semver1 = Semver.new 1, 4, 2
       semver2 = Semver.new 1, 4, 2
-      expect(semver1.equal semver2).to eq true
+      expect(semver1 == semver2).to be_truthy
     end
   end
 
   describe "major, minor, patch フィールドはゼロ、または正の整数。それ以外のものが与えられたら例外が発生" do
+    describe "major, minor, patch フィールドはゼロ、または正の整数" do
+      it "バージョンオブジェクト生成時に major, minor, patch に 0 を与えると例外が発生しない" do
+        expect { Semver.new 0, 0, 0 }.not_to raise_error
+      end
 
-    it "バージョンオブジェクト生成時に major, minor, patch に 0 を与えると例外が発生しない" do
-      expect { Semver.new 0, 0, 0 }.not_to raise_error
+      it "バージョンオブジェクト生成時に major, minor, patch に 1 を与えると例外が発生しない" do
+        expect { Semver.new 1, 1, 1 }.not_to raise_error
+      end
     end
 
-    it "バージョンオブジェクト生成時に major, minor, patch に 1 を与えると例外が発生しない" do
-      expect { Semver.new 1, 1, 1 }.not_to raise_error
+    describe "それ以外のものが与えられたら例外が発生" do
+      it "バージョンオブジェクト生成時に major, minor, patch に -1 を与えると例外が発生する" do
+        expect { Semver.new -1, -1, -1 }.to raise_error(ArgumentError)
+      end
+
+      it "バージョンオブジェクト生成時に major, minor, patch に 1.5 を与えると例外が発生する" do
+        expect { Semver.new 1.5, 1.5, 1.5 }.to raise_error(ArgumentError)
+      end
+
+      it " バージョンオブジェクト生成時に major, minor, patch に 文字列 'a', 'b', 'c'を与えると例外が発生する" do
+        expect { Semver.new 'a', 'b', 'c' }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe "semver におけるバージョンアップルール" do
+    describe " パッチバージョンアップすると、patchフィールドをインクリメントする" do
+      it "バージョン 1.2.3 はバージョン 1.2.4 になる" do
+        semver = Semver.new 1, 2, 3
+        expect(semver.update_patch.to_s).to eq "1.2.4"
+      end
+
+      it "バージョン 1.2.9 はバージョン 1.2.10 になる" do
+        semver = Semver.new 1, 2, 9
+        expect(semver.update_patch.to_s).to eq "1.2.10"
+      end
     end
 
-    it "バージョンオブジェクト生成時に major, minor, patch に -1 を与えると例外が発生する" do
-      expect { Semver.new -1, -1, -1 }.to raise_error(ArgumentError)
+    describe "マイナーバージョンアップすると、 minor フィールドをインクリメントし、 patch フィールドを 0 にする" do
+      it "バージョン 1.2.3 はバージョン 1.3.0 になる" do
+        semver = Semver.new 1, 2, 3
+        expect(semver.update_minor.to_s).to eq "1.3.0"
+      end
+
+      it "バージョン 1.9.0 はバージョン 1.10.0 になる" do
+        semver = Semver.new 1, 9, 0
+        expect(semver.update_minor.to_s).to eq "1.10.0"
+      end
+    end
+
+    describe "メジャーバージョンアップすると、major フィールドをインクリメントし、 minor, および patch フィールドを 0 にする" do
+      it "バージョン 1.2.3 はバージョン 2.0.0 になる" do
+        semver = Semver.new 1, 2, 3
+        expect(semver.update_major.to_s).to eq "2.0.0"
+      end
+
+      it "バージョン 1.0.0 はバージョン 2.0.0 になる" do
+        semver = Semver.new 1, 0, 0
+        expect(semver.update_major.to_s).to eq "2.0.0"
+      end
     end
   end
 end
